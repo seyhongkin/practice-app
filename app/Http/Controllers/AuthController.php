02acb566b92a254d0928673dae152a24fe5d794f
Sey Hong <kin19seyhong@gmail.com>
2023-03-14 00:24:03 +0700

    add logout function to AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $data['password'] = bcrypt($request->password);
+
+        $user = User::create($data);
+
+        $token = $user->createToken('API Token')->accessToken;
+
+        return response(['user' => $user, 'token' => $token]);
+    }
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($data)) {
+            return response(['error_message' => 'Incorrect Details. 
+            Please try again']);
+        }
+
+        $token = auth()->user()->createToken('API Token')->accessToken;
+
+        return response(['user' => auth()->user(), 'token' => $token]);
+    }
+
+    //how to logout
+    public function logout(Request $request)
+    {
+        auth()->logout();
+
+        return response(['success' => 'Logout Successful']);
+    }
+}
